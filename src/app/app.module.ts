1fc6cb7e0c78076dadbbb79e3d5f3aa222f64234
Yesenia <1719110284@utectulancingo.edu.mx>
2022-10-11 23:25:28 -0500

    mensaje

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormulariosComponent } from './formularios/formularios.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormulariosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
